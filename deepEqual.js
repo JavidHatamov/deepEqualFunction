f5022a221e148a7d8ac9524855e478fcb2c11cb5
Javid Hatamov <cavidh.ov@mail.ru>
2022-11-15 12:39:04 +0400

    Edited

--- deepEqual.js
+++ deepEqual.js
@@ -2,18 +2,21 @@ let frontendDeveloper={
     name:"Javid",
     nickname:"Boss",
     motto:'NeverGiveUp',
-    doneTasks:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
     salary:5000,
 };
-let SeniorDeveloper={
+let seniorDeveloper={
     name:"Jamel",
     nickname:"Hugo",
     motto:'NeverGiveUp',
-    doneTasks:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
     salary:5000,
 };
-
-let backendDeveloper=frontendDeveloper;
+let backendDeveloper={
+    name:"Javid",
+    nickname:"Boss",
+    motto:'NeverGiveUp',
+    salary:5000,
+};
+//let backendDeveloper=frontendDeveloper;
 //This can be something that is confuseable, I mean when we do this action then, when we change one property in one object then this property also change in another object.
 function deepequal(a, b){
 let flag=0;
@@ -22,10 +25,10 @@ let flag=0;
             flag++;
         }
     }
-if(flag===5){
+if(flag===4){
     console.log("Yes");
 }else{
     console.log("No");
 }
 }
-deepequal(backendDeveloper,frontendDeveloper);
+deepequal(backendDeveloper,seniorDeveloper);
